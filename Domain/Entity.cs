3b3d64b9c33387589673fed96803bb29b8aefd12
bryan <bryan@BRYANPC>
2022-11-30 23:52:19 -0300

    Add project files.

---
+++ Domain/Entity.cs
@@ -0,0 +1,18 @@
+﻿namespace IWantApp.Domain;
+
+public abstract class Entity
+{
+    protected Entity()
+    {
+        this.Id = Guid.NewGuid();
+    }
+
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public string? EditedBy { get; set; }
+    public DateTime EditedOn { get; set; }
+
+
+}
---
+++ Domain/Products/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace IWantApp.Domain.Products;
+
+public class Product : Entity
+{
+    public int CategoryId { get; set; }
+    public Category? Category { get; set; }
+    public string? Description { get; set; }
+    public bool HasStock { get; set; }
+}
