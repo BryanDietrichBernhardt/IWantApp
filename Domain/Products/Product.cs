da34c17b296fc90d0571530ff83158d136ea88b6
BryanDietrichBernhardt <bryandbernhardt@gmail.com>
2022-12-01 01:09:08 -0300

    Add CategoryGetAll

--- Domain/Products/Product.cs
+++ Domain/Products/Product.cs
@@ -3,8 +3,8 @@
 public class Product : Entity
 {
     public Guid CategoryId { get; set; }
-    public Category? Category { get; set; }
-    public string? Description { get; set; }
+    public Category Category { get; set; }
+    public string Description { get; set; }
     public bool HasStock { get; set; }
     public bool Active { get; set; } = true;
 }
