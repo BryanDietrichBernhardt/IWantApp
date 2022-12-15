7e96a107d9027d155f6d6814a362f90e6ef67488
BryanDietrichBernhardt <bryandbernhardt@gmail.com>
2022-12-15 16:22:14 -0300

    Showcase pagination and ordination

--- Endpoints/Products/ProductGetShowcase.cs
+++ Endpoints/Products/ProductGetShowcase.cs
@@ -7,13 +7,29 @@ public class ProductGetShowcase
     public static Delegate Handle => Action;
 
     [AllowAnonymous]
-    public static IResult Action(ApplicationDbContext context)
+    public static IResult Action(int? page, int? row, string? orderBy, ApplicationDbContext context)
     {
-        var products = context.Products
+        if (page == null)
+            page = 1;
+        if (row == null)
+            row = 10;
+        if (string.IsNullOrEmpty(orderBy) || string.IsNullOrWhiteSpace(orderBy))
+            orderBy = "name";
+
+        var queryBase = context.Products
             .Include(p => p.Category)
-            .Where(p => p.HasStock && p.Category.Active)
-            .OrderBy(p => p.Name)
-            .ToList();
+            .Where(p => p.HasStock && p.Category.Active);
+
+        if (orderBy == "name")
+            queryBase = queryBase.OrderBy(p => p.Name);
+        else
+            queryBase = queryBase.OrderBy(p => p.Price);
+
+        var queryFilter = queryBase.Skip((page.Value - 1) * row.Value).Take(row.Value);
+
+        var products = queryFilter.ToList();
+
+
         var results = products.Select(p => new ProductResponse(p.Name, p.Category.Name, p.Description, p.HasStock, p.Price, p.Active));
 
         return Results.Ok(results);
